4fb35e97d40526a96ef42a2d689ce4a5a9f298ed
saitha vismith <sasithavismith@gmail.com>
2020-05-26 18:43:02 +0530

    view data

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,12 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Employee{
+id:string;
+name:string;
+email:string;
+password:string;
+}
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
   styleUrls: ['./aboutus.component.css']
 })
 export class AboutusComponent implements OnInit {
-
+employees: Employee[]=[
+  {
+id:"1",
+name:"sasitha", 
+email:"sasitha@gmail.com" ,
+password:"123"
+},
+{
+  id:"2",
+  name:"thilini", 
+  email:"thilini@gmail.com" ,
+  password:"1234"
+  },
+  {
+    id:"3",
+    name:"vismith", 
+    email:"vismith@gmail.com" ,
+    password:"123"
+    }
+];
   constructor() { }
 
   ngOnInit(): void {
